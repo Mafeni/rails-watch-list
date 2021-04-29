cddf0d8e1b5ab51266f256f47c7d156a14a2fef0
Robert Mafeni <robertmafeni@gmail.com>
2021-04-29 21:09:56 +0100

    functionality complete, style to be worked on

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates_uniqueness_of :movie_id, scope => [:list_id]
   validates :comment, length: { minimum: 6 }
+  validates_uniqueness_of :movie_id, scope: :list_id
 end
