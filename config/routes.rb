1b241c8d0d76c613aef882d2de149b08e0882292
Robert Mafeni <robertmafeni@gmail.com>
2021-05-04 19:17:51 +0100

    heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
+  end
+
+  resources :movies, only: %i[show] do
+    # add review model and routes
   end
 end
