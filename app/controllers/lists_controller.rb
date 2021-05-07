feab9d81b70c40ba61f9b5714861f40f0c4d86af
Robert Mafeni <robertmafeni@gmail.com>
2021-05-07 20:03:46 +0100

    site 80% done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,9 +18,27 @@ class ListsController < ApplicationController
     redirect_to lists_path
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+
+    redirect_to lists_path
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+
+    redirect_to lists_path
+  end
+
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :picture)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, only: %i[index show new create] do
+  resources :lists do
     resources :bookmarks, only: %i[new create]
+    resources :reviews, only: %i[new create]
   end
 
-  resources :movies, only: %i[show] do
-    # add review model and routes
-  end
+  resources :movies, only: %i[show]
 end
